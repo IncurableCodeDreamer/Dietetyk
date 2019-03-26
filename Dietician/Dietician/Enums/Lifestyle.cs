5f5fffb66e9dc230a8aadf0e6cc364e0362930ff
Klaudia Nowak <klaudia.nowak@euvic.pl>
2019-03-26 20:27:43 +0100

    Remove unused usings

--- Dietician/Dietician/Enums/Lifestyle.cs
+++ Dietician/Dietician/Enums/Lifestyle.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Dietician.Enums
+﻿namespace Dietician.Enums
 {
     public enum Lifestyle
     {
