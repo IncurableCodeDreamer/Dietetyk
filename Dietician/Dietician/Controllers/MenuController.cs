b2b5e7a8136d1dcbebf8b10b1c21da6297177072
Klaudia Nowak <klaudia.nowak@euvic.pl>
2019-05-14 17:04:29 +0200

    Create pdf template with mock data

--- Dietician/Dietician/Controllers/MenuController.cs
+++ Dietician/Dietician/Controllers/MenuController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Dietician.CosmosDB;
 using Dietician.Enums;
+using Dietician.Helpers;
 using Dietician.Models;
 using Dietician.Storage;
 using Dietician.Storage.Interfaces;
@@ -19,6 +20,40 @@ namespace Dietician.Controllers
         public MenuController(IAppConfiguration appConfiguration)
         {
             _repository = new RepositoryWrapper(appConfiguration);
+        }
+
+        public FileResult ExportToPdf(List<Meal> meals = null)
+        {
+            List<Meal> mealsList = new List<Meal>();
+            Array values = Enum.GetValues(typeof(MealType));
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                mealsList.Add(new Meal()
+                {
+                    Date = DateTime.Now.AddDays(i),
+                    CosmosMeal = new CosmosMealModel()
+                    {
+                        Calories = 24,
+                        Carbohydrates = 10,
+                        Fat = 10,
+                        Guid = "guid",
+                        Ingredients = "mleko, jaja, costam",
+                        Kind = "typ",
+                        Name = "owsianka",
+                        Portions = "porcje",
+                        Prepare = "nalej mleka do miski, dodaj płatki, wymieszaj w 30 stopniach przez 20 minut i wsio",
+                        Proteins = 20,
+                        Type = (MealType)values.GetValue(random.Next(values.Length)),
+                    },
+                    MealType = (MealType)values.GetValue(random.Next(values.Length)),
+                    JsonId = 1
+                });
+            }
+
+            var pdfByteArray = PdfHelper.WritePdf(mealsList);
+            return File(pdfByteArray, System.Net.Mime.MediaTypeNames.Application.Pdf);
         }
 
         public IActionResult Index()
