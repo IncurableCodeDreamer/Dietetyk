b7f1c507137f49e5dda3f71d002e7950b29de689
DziewaAngelika <dziewa@vp.pl>
2019-05-25 16:11:08 +0200

    change max date in statistic for actually month, validation for add meals, center loader, add loader before change meals for day

--- Dietician/Dietician/Models/AddMeal.cs
+++ Dietician/Dietician/Models/AddMeal.cs
@@ -6,22 +6,37 @@ namespace Dietician.Models
     public class AddMeal
     {
         public string ImageUrl { get; set; }
+
         [Required(ErrorMessage ="Pole jest wymagane")]
         public string Name { get; set; }
         public string Prepare { get; set; }
+
         [Required(ErrorMessage = "Pole jest wymagane")]
         public string Ingredients { get; set; }
+
         [Required(ErrorMessage = "Pole jest wymagane")]
         public KindsOfDiet Kind { get; set; }
+
         [Required(ErrorMessage = "Pole jest wymagane")]
         public MealType Type { get; set; }
+
+        [Range(1, double.PositiveInfinity, ErrorMessage = "Wartość musi być większa od 0")]
         [Required(ErrorMessage = "Pole jest wymagane")]
         public int Portions { get; set; }
+
+        [Range(1, double.PositiveInfinity, ErrorMessage = "Wartość musi być większa od 0")]
         [Required(ErrorMessage = "Pole jest wymagane")]
         public int Calories { get; set; }
+
+        [Range(1, double.PositiveInfinity, ErrorMessage = "Wartość musi być większa od 0")]
         public int Carbohydrates { get; set; }
+
+        [Range(1, double.PositiveInfinity, ErrorMessage = "Wartość musi być większa od 0")]
         public int Proteins { get; set; }
+
+        [Range(1, double.PositiveInfinity, ErrorMessage = "Wartość musi być większa od 0")]
         public int Fat { get; set; }
+
         public string Url { get; set; }
         
     }
